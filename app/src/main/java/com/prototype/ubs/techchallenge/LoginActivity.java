e29ede2dfe1e4fb84d272c72e287624a63ab966e
michael <mike.bdg@gmail.com>
2015-06-07 03:42:36 +0800

    Set up project and start building on login page

---
+++ app/src/main/java/com/prototype/ubs/techchallenge/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.prototype.ubs.techchallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 7/6/2015.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
