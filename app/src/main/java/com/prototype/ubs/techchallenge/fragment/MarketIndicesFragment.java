8d19f0c63c6a137fd0b3d9d0eaa7596ce47c4830
michael <mike.bdg@gmail.com>
2015-06-28 22:53:32 +0800

    Fix some bugs

--- app/src/main/java/com/prototype/ubs/techchallenge/fragment/MarketIndicesFragment.java
+++ app/src/main/java/com/prototype/ubs/techchallenge/fragment/MarketIndicesFragment.java
@@ -1,29 +1,24 @@
 package com.prototype.ubs.techchallenge.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.prototype.ubs.techchallenge.MainActivity;
 import com.prototype.ubs.techchallenge.R;
 
 /**
- * Created by Michael on 18/6/2015.
+ * Created by Michael on 28/6/2015.
  */
-public class MarketNewsFragment extends Fragment{
+public class MarketIndicesFragment extends Fragment {
     private View v;
-    private ActionBar actionBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.market_news, container, false);
+        v = inflater.inflate(R.layout.market_indices, container, false);
 
         return v;
     }
 }
-
-
