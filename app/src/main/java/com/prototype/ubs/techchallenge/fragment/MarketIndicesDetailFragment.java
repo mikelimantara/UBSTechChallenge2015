efc35351dff7895e85bbb8d801b02ab0b24abec8
michael <mike.bdg@gmail.com>
2015-07-04 01:54:40 +0800

    Link login page

--- app/src/main/java/com/prototype/ubs/techchallenge/fragment/MarketIndicesDetailFragment.java
+++ app/src/main/java/com/prototype/ubs/techchallenge/fragment/MarketIndicesDetailFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.prototype.ubs.techchallenge.MainActivity22;
 import com.prototype.ubs.techchallenge.R;
@@ -14,13 +15,24 @@ import com.prototype.ubs.techchallenge.R;
 /**
  * Created by Michael on 28/6/2015.
  */
-public class MarketIndicesDetailFragment extends Fragment {
+public class MarketIndicesDetailFragment extends Fragment implements View.OnClickListener {
     private View v;
+    private Button btnBack;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.market_indices_detail, container, false);
 
+        btnBack = (Button) v.findViewById(R.id.btn_back_market_indices);
+        btnBack.setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnBack) {
+            getActivity().getSupportFragmentManager().popBackStackImmediate();
+        }
     }
 }
